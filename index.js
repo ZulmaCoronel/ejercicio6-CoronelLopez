eb6b5e74551efa48e34d408064ebcebd7430fc8b
Zulma Coronel <zuiscoronello@ittepic.edu.mx>
2019-06-05 09:47:03 -0600

    Csv a txt

--- index.js
+++ index.js
@@ -1,16 +1,21 @@
-var fs = require ("fs");
+var fs = require('fs');
+var parse = require('csv-parse');
 
-/*fs.appendFile('alumnos.txt',
- '14401025 Zulma Isabel Coronel Lopez 90\n15400035 Ana Paola Lopez Orozco 84\n14401027 Arturo Misael Escalante Licona 95',(err,dos)=>{
-    if(err)
-    throw err;
-    console.log('Saved!');
-    
-});*/
+var datos=[];
+var cadena="";
 
-    fs.writeFile('alumnos.txt','14401025 Zulma Isabel Coronel Lopez 90\n15400035 Ana Paola Lopez Orozco 84\n14401027 Arturo Misael Escalante Licona 95\n14401038 Ana Carolina Mondragon Rangel 98\n15401057 Carlos Emmanuel Zambrano Cortez 78\n16400189 Jonathan Zavala Duarte 87\n15401046 Issia Virgen Rivera 89\n16400198 Jorge Omar Mariscal 85\n15401002 Noe Jahir Rodriguez Castillo 94\n16400138 Lizbeth Araceli Luna Ceniceros 87',(err)=>{
-        if(err)
-        throw err;
-        console.log('Saved!');
-      
-});
+fs.createReadStream('alumnos.csv')
+	.pipe(parse({delimiter:','}))
+	.on('data', function(csvrow){
+		datos.push(csvrow);
+	}).on('end', function(){
+		for (var i = 0; i < datos.length; i++) {
+			cadena+="No: "+datos[i][0] + "\n"+"NumeroControl: "+datos[i][1] + "\n"+"Nombre: "+datos[i][2] + "\n"+"Calificación: "+datos[i][3] + "\n"+"______\n";
+		}
+		fs.writeFile("alumnos.txt",cadena,function(err){
+			if(err){
+				return console.log(err);
+			}
+			console.log("Guardado");
+		});
+	});
